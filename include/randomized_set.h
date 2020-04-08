0427bed53355cdb9fcceb2cd0db6e2425edb83bc
solopointer <solopointer1202@gmail.com>
2020-04-08 19:08:45 +0800

    upload

--- include/randomized_set.h
+++ include/randomized_set.h
@@ -10,19 +10,28 @@ class randomized_set {
     std::unordered_map<K, index_t> hash_store;
     std::vector<std::pair<K, V>> vec_store;
 public:
-    void insert(const K& k, const V& v) {
-        vec_store.push_back(std::make_pair(k, v));
-        hash_store[k] = vec_store.size() - 1;
+    bool insert(const K& k, const V& v) {
+        if (auto res = hash_store.find(k); res != hash_store.end()) {
+            vec_store[res->second].second = v;
+            return false;
+        } else {
+            vec_store.push_back(std::make_pair(k, v));
+            hash_store[k] = vec_store.size() - 1;
+        }
+        return true;
     }
 
-    void erase(const K& k) {
+    bool erase(const K& k) {
         if (auto res = hash_store.find(k); res != hash_store.end()) {
             hash_store[vec_store.back().first] = res->second;
             std::swap(vec_store[res->second], vec_store[vec_store.size() - 1]);
             vec_store.pop_back();
             hash_store.erase(res);
+            return true;
         }
+        return false;
     }
+
     K& random_get() {
          std::random_device rd;
          std::mt19937 gen(rd());
--- test/randomized_set.cpp
+++ test/randomized_set.cpp
@@ -4,21 +4,23 @@
 
 int main() {
     alg::randomized_set<int, int> s;
-    s.insert(0, 1110);
-    s.insert(1, 1111);
-    s.insert(2, 1112);
-    s.insert(3, 1113);
-    s.insert(4, 1114);
-    s.insert(5, 1115);
-    s.insert(6, 1116);
-    for (auto i = 0; i < 5; i++) {
+    std::cout<<s.insert(0, 1110)<<std::endl;
+    std::cout<<s.insert(0, 2220)<<std::endl;
+    std::cout<<s.insert(1, 1111)<<std::endl;
+    std::cout<<s.insert(2, 1112)<<std::endl;
+    std::cout<<s.insert(3, 1113)<<std::endl;
+    std::cout<<s.insert(4, 1114)<<std::endl;
+    std::cout<<s.insert(5, 1115)<<std::endl;
+    std::cout<<s.insert(6, 1116)<<std::endl;
+    for (auto i = 0; i < 6; i++) {
         std::cout<<">"<<s.random_get()<<std::endl;
     }
-    s.erase(1);
-    s.erase(2);
-    s.erase(3);
-    s.erase(4);
-    s.erase(5);
-    s.erase(6);
+    std::cout<<s.erase(1)<<std::endl;
+    std::cout<<s.erase(11)<<std::endl;
+    std::cout<<s.erase(2)<<std::endl;
+    std::cout<<s.erase(3)<<std::endl;
+    std::cout<<s.erase(4)<<std::endl;
+    std::cout<<s.erase(5)<<std::endl;
+    std::cout<<s.erase(6)<<std::endl;
     return 0;
 }
