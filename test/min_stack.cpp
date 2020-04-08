ac7e3a3dd0ad04c53cce0ba925fa93515d979016
solopointer <solopointer1202@gmail.com>
2020-04-08 13:05:10 +0800

    upload

---
+++ test/min_stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+#include "log.h"
+#include "min_stack.h"
+
+
+
+int main() {
+    alg::min_stack::min_stack<int> msta;
+    std::list<int> input{5,6,4,7,3,8,1};
+    for (auto i : input) {
+        msta.push(i);
+        TRACE("push: %d, min: %d", i, msta.min());
+    }
+    while (!msta.empty()) {
+        TRACE("pop: %d, min: %d", msta.top(), msta.min());
+        msta.pop();
+    }
+    return 0;
+}
