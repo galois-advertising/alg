1209091acc5d88b84c884d872483e2fb180e7e16
solopointer <solopointer1202@gmail.com>
2020-04-14 16:58:13 +0800

    upload

--- include/list.h
+++ include/list.h
@@ -48,5 +48,50 @@ list_node_t* copy(list_node_t* list, copy_callback_t<T, list_node_t> callback = 
     return result;
 }
 
+template <typename T, typename list_node_t = default_list_node_t<T>>
+void sort(list_node_t** head, bool(*cmp)(const list_node_t*, const list_node_t*)) {
+    if (head == nullptr || *head == nullptr || (*head)->next == nullptr) {
+        return;
+    }
+    auto last_ordered = *head;
+    //                | 5 6 7 8 10 4 3 1 2
+    // last_ordered = |          ^
+    while (last_ordered != nullptr) {
+        auto first_unordered = last_ordered->next;
+        //                   | 5 6 7 8 10 4 3 1 2
+        // first_unordered = |            ^
+        if (first_unordered == nullptr) {
+            break;
+        }
+        if (cmp(last_ordered, first_unordered)) {
+            //if first_unordered is already ordered, just update the last_ordered;
+            last_ordered = first_unordered;
+        } else {
+            // find the right position for first_unordered from head;
+            // the "right position" is after "insert_position"
+            // insert_position always behind insert_position_s_next for one step
+            list_node_t* insert_position = nullptr;
+            auto insert_position_s_next = *head;
+            while(cmp(insert_position_s_next, first_unordered)) {
+                insert_position = insert_position_s_next;
+                insert_position_s_next = insert_position_s_next->next;
+            }
+            if (insert_position == nullptr) {
+                // insert_position_s_next move zero steps, so insert_position is nullptr
+                // this means we should insert it before head.
+                // and reset the *head
+                last_ordered->next = first_unordered->next;
+                first_unordered->next = *head;
+                *head = first_unordered;
+            } else {
+                // normal 
+                last_ordered->next = first_unordered->next;
+                first_unordered->next = insert_position->next;
+                insert_position->next = first_unordered;
+            }
+        }
+    }
+}
+
 
 };
--- test/list.cpp
+++ test/list.cpp
@@ -9,10 +9,17 @@ using namespace std;
 int main() {
     using node_t = alg::list::default_list_node_t<int>;
     node_t* list = nullptr;
-    std::list<int> input{1,2,3,4,5,6,7,9};
+    std::list<int> input{9, 8};
     std::for_each(input.begin(), input.end(), 
         [&list](int &i) {alg::list::insert<int>(&list, i);});
     alg::list::copy_callback_t<int> callback = [](node_t* a, node_t* b){std::cout<<a->val<<"|"<<b->val<<std::endl;};
     alg::list::copy<int>(list, callback);
+    alg::list::sort<int, node_t>(&list, [](const node_t* a, const node_t* b){return a->val <= b->val;});
+    auto root = list;
+    while (root != nullptr) {
+        TRACE(">%d", root->val);
+        root = root->next;
+    }
+
     return 0;
 }
