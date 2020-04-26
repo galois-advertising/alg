31f2dad1c0f00c6295b16b2cb1b4236f76651f8b
solopointer <solopointer1202@gmail.com>
2020-04-26 16:11:15 +0800

    upload

--- include/str.h
+++ include/str.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <list>
+#include <tuple>
 
 namespace alg::string {
 using q_t = size_t;
@@ -28,7 +30,7 @@ bool endswith(const std::string& str, const std::string& postfix) {
     return false;
 }
 
-dfa_t dfa(std::string pattern, alpha_table_t& alpha_table = default_alpha_table) {
+dfa_t generator_dfa(std::string pattern, alpha_table_t& alpha_table = default_alpha_table) {
     dfa_t dfa;
     for (q_t q = 0; q <= pattern.size(); q++) {
         for (auto ch : alpha_table) {
@@ -44,6 +46,31 @@ dfa_t dfa(std::string pattern, alpha_table_t& alpha_table = default_alpha_table)
         }
     }
     return dfa;
+}
+std::vector<std::string> search(std::string S, std::vector<std::string>& words) {
+    std::vector<std::string> result;
+    using dfa = std::tuple<q_t, q_t, dfa_t, std::string>;
+    std::list<dfa> lst;
+    for (const auto& w : words) {
+        dfa t = std::make_tuple(0, w.size(), generator_dfa(w), w);
+        lst.push_back(t);
+    }
+    for (auto ch : S) {
+        for (auto& dfa : lst) {
+            if (std::get<0>(dfa) == std::get<1>(dfa)) {
+                continue;
+            }
+            if (auto res = std::get<2>(dfa).find({std::get<0>(dfa), ch}); res != std::get<2>(dfa).end()) {
+                std::get<0>(dfa) = res->second;
+            } else {
+                std::get<0>(dfa) = 0;
+            }
+            if (std::get<0>(dfa) == std::get<1>(dfa)) {
+                result.push_back(std::get<3>(dfa));
+            }
+        }
+    }
+    return result;
 }
 
 }
--- test/str.cpp
+++ test/str.cpp
@@ -5,10 +5,14 @@ static const std::vector<alg::string::ch_t> alpha_table{
     'a','b', 'c'
 };
 int main() {
-    for (auto t : alg::string::dfa("ababaca", alpha_table)) {
+    for (auto t : alg::string::generator_dfa("ababaca", alpha_table)) {
         if (t.second != 0) {
             printf("d(%lu, %c) = %lu\n", t.first.first, t.first.second, t.second);
         }
+    }
+    std::vector<std::string> input{"a", "ab", "efg", "qwerqwer", "asdf", "def", "jkl", "aab"};
+    for (auto& str : alg::string::search("abcdefghijklmn", input)) {
+        std::cout<<str<<std::endl;
     }
 
     return 0;
